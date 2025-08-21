36761f772ad0dbde32b4f2da2bac1487593a90d3
SzaroBury <mkmateuszk@o2.pl>
2025-08-21 15:24:36 +0200

    Updated the AboutPage to the current state of the app

--- RespectCounter.ReactApp/src/Pages/AboutPage/AboutPage.jsx
+++ RespectCounter.ReactApp/src/Pages/AboutPage/AboutPage.jsx
@@ -17,58 +17,28 @@ class AboutPage extends React.Component {
                 <p className="m-5 text-center">.NET + React application that allows users to express their opinion on various public figures, their quotes and actions.</p>
 
                 <div>
-                    <h5>Target main functionalities:</h5>
+                    <h5>Main functionalities:</h5>
                     <ul>
-                        <li>Users can propose public figures.</li>
-                        <li>Users can add quotes/activities and link them to stored public figures.</li>
+                        <li>Users can propose persons.</li>
+                        <li>Users can add quotes/actions and link them to stored persons.</li>
                         <li>Users can comment and react to persons, activities and other comments.</li>
                         <li>Users can search quotes, activities and persons by tags.</li>
-                        <li>Users can report activities, comments.</li>
-                        <li>Moderators can verify public figures, quotes and activities added by users.</li>
-                        <li>Moderators can create verified public figures, quotes and activities.</li>
-                        <li>Moderators can hide comments and activities.</li>
+                        <li>Admin can verify persons, quotes and activities added by users.</li>
+                        <li>Admin can create verified persons, quotes and activities.</li>
+                        <li>Admin can hide persons, activities and comments.</li>
                     </ul>
                 </div>
                 <h5>To-do list: </h5>
-                    <ul>
-                        <li>User:
-                            <ul>
-                                <li>Singing up</li>
-                                <li>Singing in</li>
-                            </ul>
-                        </li>
-                        <li>Events:
-                            <ul>
-                                <li>List of events:
-                                    <ul>
-                                        <li>Get list from API - DONE</li>
-                                        <li>Sort list - DONE</li>
-                                        <li>Change scope of list - DONE</li>
-                                        <li>Tags:
-                                            <ul>
-                                                <li>Get list of tags - DONE</li>
-                                                <li>Get event with the tag</li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>Detailed view</li>
-                                <li>Rating</li>
-                            </ul>
-                        </li>
-                        <li>Persons:
-                            <ul>
-                                <li>List of the persons</li>
-                                <li>Detailed view</li>
-                                <li>Images</li>
-                            </ul>
-                        </li>
-                        <li>Home page:
-                            <ul>
-
-                            </ul>
-                        </li>
-                    </ul>
+                <ul>
+                    <li>Avatar images</li>
+                    <li>User settings</li>
+                    <li>Reporting activities, comments...</li>
+                    <li>More data for testing</li>
+                </ul>
+                <h5>Errors to fix:</h5>
+                <ul>
+                    <li></li>
+                </ul>
             </Container>
         );
     }
