5fe66559f49de36f0edfb1c74ccb845557b1342f
SzaroBury <mkmateuszk@o2.pl>
2026-02-26 00:07:35 +0100

    Refactor: Update ReadOnlyRepository to use Application layer contracts and simplify methods. - Removed System.Linq.Expressions from repository interfaces to prevent persistence leakage into the Application layer. - Relocated the ExistsAsync logic from the EntityChecker service.

--- RespectCounter.Infrastructure/Repositories/ReadOnlyRepository.cs
+++ RespectCounter.Infrastructure/Repositories/ReadOnlyRepository.cs
@@ -1,7 +1,7 @@
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using RespectCounter.Domain.Model;
-using RespectCounter.Domain.Contracts;
+using RespectCounter.Application.Shared.Contracts;
 
 namespace RespectCounter.Infrastructure.Repositories;
 
@@ -14,94 +14,23 @@ public class ReadOnlyRepository : IReadOnlyRepository
         this.dbContext = dbContext;
     }
 
-    public async Task<T?> FindByIdAsync<T>(
+    public Task<T?> FindByIdAsync<T>(
         Guid id,
         CancellationToken cancellationToken = default
     ) where T : Entity
     {
-        return await dbContext.Set<T>().FindAsync([id], cancellationToken: cancellationToken);
+        return dbContext.Set<T>().AsNoTracking().FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
     }
 
-    public IQueryable<T> FindQueryable<T>(
-        Expression<Func<T, bool>> filter,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null
-    ) where T : Entity
+    public async Task<bool> ExistsAsync<T>(Guid entityId, CancellationToken cancellationToken) where T : Entity
     {
-        var query = dbContext.Set<T>().Where(filter);
-        return orderBy != null ? orderBy(query) : query;
+        return await dbContext.Set<T>().AsNoTracking().AnyAsync(entity => entity.Id == entityId);
     }
 
     public Task<List<T>> FindListAsync<T>(
-        Expression<Func<T, bool>>? filter,
-        string[]? includeProperties = null,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
         CancellationToken cancellationToken = default
     ) where T : class
     {
-        IQueryable<T> query;
-
-        if (filter == null)
-        {
-            query = dbContext.Set<T>();
-        }
-        else
-        {
-            query = dbContext.Set<T>().Where(filter);
-        }
-
-        if (includeProperties != null)
-        {
-            foreach (string prop in includeProperties)
-            {
-                query = query.Include(prop);
-            }
-        }
-
-        if (orderBy != null)
-        {
-            query = orderBy(query);
-        }
-
-        return  query.ToListAsync(cancellationToken);
-    }
-
-    public Task<List<T>> FindListAsync<T>(
-        IQueryable<T> query,
-        string[]? includeProperties = null,
-        Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-        CancellationToken cancellationToken = default
-    ) where T : class
-    {
-        if (includeProperties != null)
-        {
-            foreach (string prop in includeProperties)
-            {
-                query = query.Include(prop);
-            }
-        }
-
-        if (orderBy != null)
-        {
-            query = orderBy(query);
-        }
-
-        return query.ToListAsync(cancellationToken);
-    }
-
-    public Task<T?> SingleOrDefaultAsync<T>(
-        Expression<Func<T, bool>> filter,
-        string? includeProperties = null,
-        CancellationToken cancellationToken = default
-    ) where T : Entity
-    {
-        var query = dbContext.Set<T>().AsQueryable();
-
-        if (!string.IsNullOrWhiteSpace(includeProperties))
-        {
-            query = includeProperties.Split([','], StringSplitOptions.RemoveEmptyEntries)
-                .Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
-        }
-
-        return query.SingleOrDefaultAsync(filter, cancellationToken);
+        return dbContext.Set<T>().AsNoTracking().ToListAsync(cancellationToken);
     }
 }
