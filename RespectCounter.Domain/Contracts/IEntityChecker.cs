c8cfd7738c9497fa0ec7040ae4b6e3abc241e89e
SzaroBury <mkmateuszk@o2.pl>
2026-02-26 00:09:37 +0100

    Remove IEntityChecker interface and EntityChecker implementation as they are not used anymore

--- RespectCounter.Domain/Contracts/IEntityChecker.cs
+++
@@ -1,8 +0,0 @@
-using RespectCounter.Domain.Model;
-
-namespace RespectCounter.Domain.Contracts;
-
-public interface IEntityChecker
-{
-    Task<bool> ExistsAsync<T>(Guid id, CancellationToken cancellationToken) where T : Entity;
-}
